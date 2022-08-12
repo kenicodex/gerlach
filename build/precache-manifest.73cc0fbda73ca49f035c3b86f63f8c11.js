36d86e833f8ba4ebbb53f70c903cbb1fc987b15b
kenicodex <kehindesalaudeen222@gmail.com>
2022-08-12 01:46:06 +0100

    adding ignored files

---
+++ build/precache-manifest.73cc0fbda73ca49f035c3b86f63f8c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e143c1c95ac96f0f18aa",
+    "url": "/static/css/main.c1e3e7ca.chunk.css"
+  },
+  {
+    "revision": "e143c1c95ac96f0f18aa",
+    "url": "/static/js/main.e143c1c9.chunk.js"
+  },
+  {
+    "revision": "56dbc343fe2dac40479d",
+    "url": "/static/js/1.56dbc343.chunk.js"
+  },
+  {
+    "revision": "d0627070471703610ed8",
+    "url": "/static/js/2.d0627070.chunk.js"
+  },
+  {
+    "revision": "cfbbace2586378dd3871",
+    "url": "/static/js/runtime~main.cfbbace2.js"
+  },
+  {
+    "revision": "ba7489c5743256aa19f5557092a55a91",
+    "url": "/index.html"
+  }
+];
